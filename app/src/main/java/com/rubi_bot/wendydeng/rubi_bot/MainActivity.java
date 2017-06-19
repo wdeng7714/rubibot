d6701c6bcd11a08c0ed74c747a1c395dd4198140
wendydeng <wendy9852@hotmail.com>
2017-06-18 20:39:13 -0400

    Recognizes face cube colors as a string

--- app/src/main/java/com/rubi_bot/wendydeng/rubi_bot/MainActivity.java
+++ app/src/main/java/com/rubi_bot/wendydeng/rubi_bot/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.drawable.Drawable;
 import android.provider.MediaStore;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +19,12 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import java.util.*;
 
 import java.net.URL;
-
+enum FACES{
+    Up, Right, Front, Down, Left, Bottom
+}
 public class MainActivity extends Activity {
     int BUTTON_HEIGHT = 700;
     int SCREEN_HEIGHT = 2560;
@@ -28,16 +32,25 @@ public class MainActivity extends Activity {
     Button[] captureButton = new Button[6];
     Bitmap[] cubeSideImages = new Bitmap[6];
     Button btnSolve;
+    Pair<String, Integer>[] COLORS = new Pair[6];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        COLORS[0] = new Pair("RED", Color.rgb(255, 0, 0));
+        COLORS[1] = new Pair("GREEN", Color.rgb(0, 255, 0));
+        COLORS[2] = new Pair("BLUE", Color.rgb(0, 0, 255));
+        COLORS[3] = new Pair("YELLOW", Color.rgb(255, 255, 0));
+        COLORS[4] = new Pair("ORANGE", Color.rgb(255, 102, 0));
+        COLORS[5] = new Pair("WHITE", Color.rgb(255, 255, 255));
+
+
         setContentView(R.layout.activity_main);
         LinearLayout l1 = (LinearLayout)findViewById(R.id.l1);
         LinearLayout l2 = (LinearLayout)findViewById(R.id.l2);
         for (int i = 0; i < 6; i++){
-
+            //Order the sides are read: Up Right Front Down Left Bottom
             captureButton[i] = new Button(this);
-            captureButton[i].setText(String.format("Side %d", i));
+            captureButton[i].setText(String.format("Side %s", FACES.values()[i]));
             captureButton[i].setId(i);
             captureButton[i].setHeight(BUTTON_HEIGHT);
 
@@ -89,56 +102,70 @@ public class MainActivity extends Activity {
                 return true;
             }
         });
-
     }
     private void bitmapToString(Bitmap[] bmpSides){
         System.out.println("Solving");
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         StringBuilder color;
-        for(int i = 0; i < 6; i++){
-            color = getColorsSide(bmpSides[i]);
-        }
+        //for(int i = 0; i < 6; i++){
+            color = getColorsSide(bmpSides[0]);
+        //}
     }
     private StringBuilder getColorsSide(Bitmap bmpSide){
         StringBuilder color = new StringBuilder();
-        int height = bmpSide.getHeight();
         int width = bmpSide.getWidth();
-        for(int i = 0; i < height; i += 2){
-            for(int j = 0; j < width; j +=2){
-                char pixelColor = getColorsPixel(bmpSide.getPixel(j, i));
+        int faceWidth = bmpSide.getWidth()/3;
+        System.out.println("facewidth = "+faceWidth);
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++) {
+                int x = width / 6;
+                x += j * faceWidth;
+                int y = width / 6;
+                y += i * faceWidth;
+                System.out.println("x = " + x + ", y = " + y);
+                char pixelColor = getColorsPixel(bmpSide.getPixel(x, y));
                 color.append(pixelColor);
             }
-            color.append('\n');
         }
         System.out.println(color);
         return color;
     }
     private char getColorsPixel(int pixel){
-        int r = Color.red(pixel);
-        int g = Color.green(pixel);
-        int b = Color.blue(pixel);
-        if (r >= 180 && g < 50 && b < 50){
+        Pair match = getEuclidianDist(pixel);
+        if (match.first == "RED"){
             return 'R';
         }
-        else if(r < 50 && g >= 180 && b < 50){
+        else if (match.first == "GREEN"){
             return 'G';
         }
-        else if(r < 50 && g < 50 && b >= 180){
+        else if (match.first == "BLUE"){
             return 'B';
         }
-        else if(r >= 230 && g > 100 && g < 130 && b <30){
+        else if (match.first == "ORANGE"){
             return 'O';
         }
-        else if(r >=240 && g > 240 && b < 30){
+        else if (match.first == "YELLOW"){
             return 'Y';
         }
-        else if(r > 230 && g > 230 && b > 230){
+        else if (match.first == "WHITE"){
             return 'W';
         }
         return '-';
     }
-
+    private Pair getEuclidianDist(int pixel){
+        double minDist = Double.MAX_VALUE;
+        Pair closest = null;
+        for(int i = 0; i < 6; i ++){
+            double dist = 0;
+            dist = Math.pow((Color.red(pixel) - Color.red(COLORS[i].second)), 2);
+            dist += Math.pow((Color.green(pixel) - Color.green(COLORS[i].second)), 2);
+            dist += Math.pow((Color.blue(pixel) - Color.blue(COLORS[i].second)), 2);
+            if (dist < minDist){
+                minDist = dist;
+                closest = COLORS[i];
+            }
+        }
+        return closest;
+    }
     private void dispatchTakePictureIntent(int side) {
         System.out.println(side);
         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -152,6 +179,7 @@ public class MainActivity extends Activity {
         if (resultCode == RESULT_OK){
             Bundle extras = data.getExtras();
             Bitmap capturedImage = (Bitmap) extras.get("data");
+            capturedImage = Bitmap.createBitmap(capturedImage, 0,0, capturedImage.getWidth(), capturedImage.getWidth());
             cubeSideImages[requestCode] = capturedImage;
             captureButton[requestCode].setBackground(new BitmapDrawable(getResources(), capturedImage));
     }
