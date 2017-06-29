7276cc9693326e23bccf619a4054f95bda487771
wendydeng <wendy9852@hotmail.com>
2017-06-29 00:26:29 -0400

    created client socket

--- app/src/main/java/com/rubi_bot/wendydeng/rubi_bot/MainActivity.java
+++ app/src/main/java/com/rubi_bot/wendydeng/rubi_bot/MainActivity.java
@@ -1,31 +1,29 @@
 package com.rubi_bot.wendydeng.rubi_bot;
 
 import android.app.Activity;
-import android.support.design.widget.Snackbar;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.provider.MediaStore;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.provider.MediaStore;
+import android.support.design.widget.Snackbar;
 import android.util.Pair;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import java.util.*;
 
-import java.net.URL;
-enum FACES{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+enum FACES {
     Up, Right, Front, Down, Left, Bottom
 }
+
 public class MainActivity extends Activity {
     int BUTTON_HEIGHT = 700;
     Button[] captureButton = new Button[6];
@@ -44,11 +42,10 @@ public class MainActivity extends Activity {
         COLORS[4] = new Pair("ORANGE", Color.rgb(255, 102, 0));
         COLORS[5] = new Pair("WHITE", Color.rgb(255, 255, 255));
 
-
         setContentView(R.layout.activity_main);
-        LinearLayout l1 = (LinearLayout)findViewById(R.id.l1);
-        LinearLayout l2 = (LinearLayout)findViewById(R.id.l2);
-        for (int i = 0; i < 6; i++){
+        LinearLayout l1 = (LinearLayout) findViewById(R.id.l1);
+        LinearLayout l2 = (LinearLayout) findViewById(R.id.l2);
+        for (int i = 0; i < 6; i++) {
             //Order the sides are read: Up Right Front Down Left Back
             captureButton[i] = new Button(this);
             captureButton[i].setText(String.format("Side %s", FACES.values()[i]));
@@ -66,9 +63,9 @@ public class MainActivity extends Activity {
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             if (i % 2 == 0) {
-                l1.addView(captureButton[i],lp);
+                l1.addView(captureButton[i], lp);
             } else {
-                l2.addView(captureButton[i],lp);
+                l2.addView(captureButton[i], lp);
             }
         }
         btnSolve = (Button) findViewById(R.id.btnSolve);
@@ -76,9 +73,12 @@ public class MainActivity extends Activity {
         btnSolve.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                for(int i = 0; i < 6; i++){
-                    if(cubeSideImages[i] == null){
+                for (int i = 0; i < 6; i++) {
+                    if (cubeSideImages[i] == null) {
                         String errMsgStr = "Please fill out all sides of the cube";
+//                        new ServerConnection().execute();
+                        new ServerConnection().execute();
+//                        new ServerConnection().execute("THISISASAMPLECUBESTRING");
                         Snackbar errMsg = Snackbar.make(view.findViewById(R.id.btnSolve), errMsgStr, Snackbar.LENGTH_SHORT);
                         errMsg.show();
                         System.out.println(errMsgStr);
@@ -106,18 +106,21 @@ public class MainActivity extends Activity {
 //            }
 //        });
     }
-    private void bitmapToString(Bitmap[] bmpSides){
+
+    private void bitmapToString(Bitmap[] bmpSides) {
         System.out.println("Solving");
-        for(int i = 0; i < 6; i++){
+        for (int i = 0; i < 6; i++) {
             color = getColorsSide(bmpSides[0]);
         }
+//        getSolution(color);
     }
-    private StringBuilder getColorsSide(Bitmap bmpSide){
+
+    private StringBuilder getColorsSide(Bitmap bmpSide) {
         int width = bmpSide.getWidth();
-        int faceWidth = bmpSide.getWidth()/3;
-        System.out.println("facewidth = "+faceWidth);
-        for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++) {
+        int faceWidth = bmpSide.getWidth() / 3;
+        System.out.println("facewidth = " + faceWidth);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 int x = width / 6;
                 x += j * faceWidth;
                 int y = width / 6;
@@ -130,43 +133,41 @@ public class MainActivity extends Activity {
         System.out.println(color);
         return color;
     }
-    private char getColorsPixel(int pixel){
+
+    private char getColorsPixel(int pixel) {
         Pair match = getEuclidianDist(pixel);
-        if (match.first == "RED"){
+        if (match.first == "RED") {
             return 'R';
-        }
-        else if (match.first == "GREEN"){
+        } else if (match.first == "GREEN") {
             return 'G';
-        }
-        else if (match.first == "BLUE"){
+        } else if (match.first == "BLUE") {
             return 'B';
-        }
-        else if (match.first == "ORANGE"){
+        } else if (match.first == "ORANGE") {
             return 'O';
-        }
-        else if (match.first == "YELLOW"){
+        } else if (match.first == "YELLOW") {
             return 'Y';
-        }
-        else if (match.first == "WHITE"){
+        } else if (match.first == "WHITE") {
             return 'W';
         }
         return '-';
     }
-    private Pair getEuclidianDist(int pixel){
+
+    private Pair getEuclidianDist(int pixel) {
         double minDist = Double.MAX_VALUE;
         Pair closest = null;
-        for(int i = 0; i < 6; i ++){
+        for (int i = 0; i < 6; i++) {
             double dist = 0;
             dist = Math.pow((Color.red(pixel) - Color.red(COLORS[i].second)), 2);
             dist += Math.pow((Color.green(pixel) - Color.green(COLORS[i].second)), 2);
             dist += Math.pow((Color.blue(pixel) - Color.blue(COLORS[i].second)), 2);
-            if (dist < minDist){
+            if (dist < minDist) {
                 minDist = dist;
                 closest = COLORS[i];
             }
         }
         return closest;
     }
+
     private void dispatchTakePictureIntent(int side) {
         System.out.println(side);
         Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -174,16 +175,41 @@ public class MainActivity extends Activity {
             startActivityForResult(camera_intent, side);
         }
     }
+
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         System.out.println();
-        if (resultCode == RESULT_OK){
+        if (resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap capturedImage = (Bitmap) extras.get("data");
-            capturedImage = Bitmap.createBitmap(capturedImage, 0,0, capturedImage.getWidth(), capturedImage.getWidth());
+            capturedImage = Bitmap.createBitmap(capturedImage, 0, 0, capturedImage.getWidth(), capturedImage.getWidth());
             cubeSideImages[requestCode] = capturedImage;
             captureButton[requestCode].setBackground(new BitmapDrawable(getResources(), capturedImage));
+        }
     }
-}
+
+    class ServerConnection extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            final String host = "192.168.1.13";
+            final int port = 8080;
+            try {
+                System.out.println("Creating socket connection to " + host + port);
+                Socket sock = new Socket(host, port);
+                BufferedReader br = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+                out.print("hello");
+                String solution = br.readLine();
+                System.out.println("server says:" + solution);
+                sock.close();
+            } catch (Exception e) {
+                System.out.println("Unable to connect to solution server " + e.toString());
+                e.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+
+    }
 }
 
